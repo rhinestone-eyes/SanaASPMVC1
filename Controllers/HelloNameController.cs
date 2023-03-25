1e4f010d9291699c841c01650d02200c030015c8
Dmytro Burylko <sana22_bdv@student.ztu.edu.ua>
2023-03-25 13:33:36 +0200

    task is done

---
+++ Controllers/HelloNameController.cs
@@ -0,0 +1,30 @@
+﻿using HelloName.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MvcMovie.Controllers;
+
+public class HelloNameController : Controller
+{
+    private readonly ILogger<HelloNameController> _logger;
+
+    public HelloNameController(ILogger<HelloNameController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Greetings(helloName _name)
+    {
+        return View(_name);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/HelloName.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloName.Models
+{
+    public class helloName
+    {
+        public string Name { get; set; }
+    }
+}
